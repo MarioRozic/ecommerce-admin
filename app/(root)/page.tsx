836893ded0cecb49dea3320483633003bc8fdb49
Mario Rozic <mario.rozic@rokolabs.com>
2024-02-20 13:58:56 +0100

    :sparkles: Adding modal provider

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,24 @@
+"use client"
 import { Button } from "@/components/ui/button"
+import { Modal } from "@/components/ui/modal"
+import { useStoreModal } from "@/hooks/use-store-modal"
 import { UserButton } from "@clerk/nextjs"
 import Image from "next/image"
+import { useEffect } from "react"
 
 export default function SetupPage() {
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [isOpen, onOpen])
+
   return (
     <>
-      <div className="p-4">this is a protected route</div>
-      <UserButton afterSignOutUrl="/" />
+      <div className="p-4">Root page</div>
     </>
   )
 }
